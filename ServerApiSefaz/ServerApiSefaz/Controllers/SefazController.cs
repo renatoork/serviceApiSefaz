0ed01d3f9f2dfb6dd0927cfcd33d56449b8d1c37
Renato Arruda <renato@mega.com.br>
2019-03-22 18:17:45 -0300

    servico sefaz teste

---
+++ ServerApiSefaz/ServerApiSefaz/Controllers/SefazController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServerApiSefaz.Services;
+
+namespace ServerApiSefaz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SefazController : ControllerBase
+    {
+        private SefazService _service;
+
+        public SefazController(SefazService sefaz)
+        {
+            _service = sefaz;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            try
+            {
+                var doc = _service.GetDocumento();
+                return Ok(doc);
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ ServerApiSefaz/ServerApiSefaz/Services/SefazService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApiSefaz.Services
+{
+    public class SefazService
+    {
+        public  string GetDocumento()
+        {
+            return "<doc>00001</doc>";
+        }
+    }
+}
